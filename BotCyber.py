129dbebc1ac182361f7f5dad53022182594f8bbd
Mang Erlan <30397027+Alandworm@users.noreply.github.com>
2017-11-13 02:44:01 +0700

    Update BotCyber.py

--- BotCyber.py
+++ BotCyber.py
@@ -2467,6 +2467,51 @@ def nameUpdate():
 thread2 = threading.Thread(target=nameUpdate)
 thread2.daemon = True
 thread2.start()
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('i4CLq4v9GPwm/OqLOguDUwdQAHVAJ1tT9TSJtElimNT9p2ew/PEv6mlo1Dcp+ryKZpHMFP0CXSvaNyJggB5vd5jjzFfbdoueB6b3dL7CTiIJbaldDNFXrpXwo0XWoXa1mPDQBwpU1YjoL2CglHg8wAdB04t89/1O/w1cDnyilFU=')   #I put my TOKEN
+handler = WebhookHandler('5d96f0acf017faa0337ae4bc78bfb33b')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
 
 while True:
     try:
